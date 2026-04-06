e385e02a9fedb1d4c58d9c69d443f3cbb770fc74
picker770 <arehman897@gmail.com>
2026-04-06 19:03:19 +0100

    Connect to Postgresql database and prepare for deployment

--- codestar2/settings.py
+++ codestar2/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-3)ygu5((2+oq=y$f^x5ool93o3j98vxo5_k-^)$%27za2x@3=h"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com',
                  '127.0.0.1',]
