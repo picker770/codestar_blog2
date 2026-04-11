59ffd8c8bd5a93177eb34b5cc12e00a237b33f26
picker770 <arehman897@gmail.com>
2026-04-11 17:44:04 +0100

    debugging done

--- blog2/models.py
+++ blog2/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    
 
     class Meta:
         ordering = ["-created_on"]
@@ -33,6 +34,7 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    
 
     class Meta:
         ordering = ["created_on"]
