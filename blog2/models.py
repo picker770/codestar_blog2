b4bc632727642289523e570b97289735846cf9ac
picker770 <arehman897@gmail.com>
2026-04-11 14:22:43 +0100

    added meta class and model methods

--- blog2/models.py
+++ blog2/models.py
@@ -15,6 +15,13 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | Written by {self.author}"
+
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
